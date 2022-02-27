f777701ee6b7e4da5060b787bcb84e63e5251638
hamed <lohi.hamed@yahoo.com>
2022-02-27 10:36:03 +0330

    Add Queries Tests

---
+++ Mc2.CrudTest.AcceptanceTests/Customers/Queries/GetCustomerByIdTests.cs
@@ -0,0 +1,50 @@
+﻿using Application.Customers.Commands.CreateCustomer;
+using Application.Customers.Queries.GetCustomerById;
+using Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.AcceptanceTests.Customers.Queries
+{
+
+    using static Testing;
+
+    public class GetCustomerByIdTests : TestBase
+    {
+        [Test]
+        public async Task ShouldReturnNull()
+        {
+            var query = new GetCustomerByIdQuery() { Id = 100 };
+
+            var result = await SendAsync(query);
+
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public async Task ShouldReturnOne()
+        {
+
+            var customer = new CreateCustomerCommand()
+            {
+                Firstname = "Hamed",
+                Lastname = "Lohi",
+                Email = "lohi.hamed@gmail.com",
+                DateOfBirth = DateTime.Parse("1990-02-25"),
+                PhoneNumber = "+9809149012500",
+                BankAccountNumber = "123456789"
+            };
+
+            var itemId = await SendAsync(customer);
+
+            var query = new GetCustomerByIdQuery() { Id = itemId };
+
+            var result = await SendAsync(query);
+
+            result.Should().NotBeNull();
+        }
+    }
+}
---
+++ Mc2.CrudTest.AcceptanceTests/Customers/Queries/GetCustomersWithPaginationTests.cs
@@ -0,0 +1,51 @@
+﻿using Application.Customers.Commands.CreateCustomer;
+using Application.Customers.Queries.GetCustomersWithPagination;
+using Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.AcceptanceTests.Customers.Queries
+{
+
+    using static Testing;
+
+    public class GetCustomersWithPaginationTests : TestBase
+    {
+        [Test]
+        public async Task ShouldReturnEmptyList()
+        {
+            var query = new GetCustomersWithPaginationQuery();
+
+            var result = await SendAsync(query);
+
+            result.Items.Should().BeEmpty();
+        }
+
+        [Test]
+        public async Task ShouldReturnAllLists()
+        {
+
+            var customer = new CreateCustomerCommand()
+            {
+                Firstname = "Hamed",
+                Lastname = "Lohi",
+                Email = "lohi.hamed@gmail.com",
+                DateOfBirth = DateTime.Parse("1990-02-25"),
+                PhoneNumber = "+9809149012500",
+                BankAccountNumber = "123456789"
+            };
+
+            var itemId = await SendAsync(customer);
+
+            var query = new GetCustomersWithPaginationQuery();
+
+            var result = await SendAsync(query);
+
+            result.Items.Should().HaveCount(1);
+            //result.Items.First().Items.Should().HaveCount(7);
+        }
+    }
+}
